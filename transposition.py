29f20baadd7b657a8df0198eae2ced977db87146
Joe <joe.bell34@btinternet.com>
2018-12-06 16:26:28 +0000

    A system of niceness

--- transposition.py
+++ transposition.py
@@ -1,6 +1,7 @@
 
 import itertools
-LIKELY_WORDS = ["CONSTANTINOPLE"]
+
+LIKELY_WORDS = ["THE", "AND", "THA", "SHADOW"]
 
 def decrypt_rows(ciphertext, order):
     kwlen = len(order)
@@ -65,13 +66,34 @@ def try_find_anagram(ciphertext, kwlen):
             order_counts[recommended_order] = 1
     print(order_counts)
     return (sorted(order_counts.items(), key=lambda kv: kv[1])[-1]) if len(order_counts) else ((), 0)
-        
 
-ciphertext = """HCOOT HEAOT DHEYM AAUHA MARRA RWBEH WNDNE ESEER ETNTI RWHAT NRTRY FMRSO VEHSB RNAUT SELTE GOUEG SDENH HEYDE SEERI OEUAI SHVLS RDAEO MACDN IFDNE IAEAS EANFO AHEEA ELNFM SREHI KHABT ICLIT CTFPV ETEII AHSIS PODIS GLLSI TOIIA DHLVA OTTGE OIIHT OGEES NGURF ISRRT TUUNB ANECS DAETS SGICE LTERV MHCIE ITCAO HIOTC ESTMO TWLPT AWRAN EINWC ELUON INISS SNHFH LSTDN CSTAJ AODHE AAOTE FEADT TSEWS PESAA HHMDO OEOET EETSF HORAI OLNYO EITUI EESSA UACET NAEER EVECT IEYNO HEOEH ECHEI SEEEI NAHAU EOUAF EORSO NNNEC UMFRS IRNSI UEOIE RTUCI REIAZ IOINP FRTTE FEPHI OENCS TLTET TCHWE RCLIF DTTTT ATLUB WTLLH ESRUE MNWAN ISOFM RSOFC CIARL SSTSR DCIIR NTWEE ONETP FIEEE ISNRE GNELI IECNN ENGUE RGNIN CTNEN EDEVL CEGTS CIIAN TAIOW OTMFO EHNTA SOEOR ITURA ILTDI HURMR ACUAE ALLSE GRSGU ENAEE ITTAO TTUMT OLLRA EGDTI TSRET RASCL NOEEC DIFDC MWCEA GPAAS TSTGO ATAHA FDRTE EYEBH DMWID NAAGU STRRI EAABO NEAGO OTEIE IEOSA NANRA ROLII NRETT ELEHE TNADI CANTO DOIUO NMGIO SYEIS ANIRF MSMEH CEOSH CRHRH AERRG GWTEA TTLEO NETDE NSEFH SHMRH LIHOE TESIN DEUHG TNLHT SFOGT SRNRC TWTTV RMHSO NTEOG DUARA AOORI IPUNN NHESD ARCHL SHPIE TANTR INLST LHSIV ISORO STOYM OASAT LEINT EFCOD ITAIA ATSST NANIU NNSAI FOSMR SPRDS ECNOO OLODO TUEET TENIY TIRSV RONTL ETTMP ANTHA IODCD EUDHE AIAED SWIVE TEFTE LEFTH GGTSE TTDDT HRNAY COSDP ETORC EOFOO EEOSA OEITU TRTRH SMWED CSNTN NIRNC HAISE SIRNH ITAOH GEEFA IREYI TOVOA EEOER EFASS ISTNA BFTBN POROI EMARI NOREI TRTSN LAWII IEOEN MMUER EHHCO CSOLF MVVTI OEHEA HNLWU EOORU LRHFN OLIXA AOIHE WABAH ANUNG RLONI VCTRA IIRBC RACAT MEPTD PUSML NNPTF PGONU RCGTI UAROA SAORD RCMMS IEHNE OOSNI LERHF NEROI EHFIO IDPIF RHTEN STEIT GETHI OLGAS GSERI SGHEO AGDIE LGNTR EGTNE ERROT NAUMW TPYWG TRDKT EPSDD HDEWG MEONP LTAHA INDID ETTHU EHESN GAABN GWFTA ORTCA DSRLS AEOFN NEDTT KERSR OTUTT HOENE ODMME ILHTE DANTI THOFE TEETL NTLHC MSDIN VRNNR ALTHI NEEYA OIENN ERSNU NTUCI UVEER BLOEF MIERE ODEAI TSUEG AUTIE SMSEI IRSOB IAEGF TNGEH EOOGR AHUOR PNMAL DSOAV GRIAS ADDTM FKEAE HAARI ITMLS TIERI EOAEH EANWT GLOEA HRROL EATEN TUTOH AONLH SESMH ATTFB NNCET RNOYD VHEEE STITT ROERE POOTN AIETS UEEAT OISSR SNETL SGGTH NDCIV RTRCN PMCSA ENTTR ESVES NRSNO NEELE IDTNN ELOTC NFNTI CKOTP GRBFF DICMT RKEUE OERIW EAIEA NEIOO TEROI RYPNF TETSE EUIOP WODTB HFSAT IOUET TTHCN NNLEI LONDI CTRNT OAYGH ITSSU NTOII ETEHI TEFEN HADEE ERONR AIESL ECURA NPOUU IEEEI KNANI OEEIE OORNS NFOMT IAEAO EUNBI KTENO NRIUD NCIHR RANWA HTTOA LNNTT EOPNN NMNFO TAHAU AWEDN REIRH PRWTN SLFSU NCLWH ANVRE REROH NNIDN EUFNI POESC ELTIS DNASR ESNNT EMEIR VNADO BPWSD OEEIR SOTNR SHSON IXNAE ANSSL OAOHA UPAIR NFOAE HLEAA HEEAT DCYST FHIHP NILRR ITEUE RAORS TTOJD MTTAI SCTEO USCAE TIIOG AEGNI CHAEF MFIAL ORIOR NCSNF OCROS LFROO GDALH ETSMG URENW SSWEI ONRWS ETTMS RRNNI WESTV TFEON TBTUE OOLAN ECREN ESEIM NHSNI LWDTT RGTAR SOLID NHENK FRTTW TLOSN FOPHC HGTAH ECBOA ORMT""".replace(" ", "")
-for kwlen in range(3, 12):
-    x = ''.join(rows_from_columns(ciphertext, kwlen))
-    c, _ = try_find_anagram(x, kwlen)
-    print(kwlen, c)
-    print("HELLO")
-    if c is not ():
-        print(decrypt_rows(x, c)[:20])
+def brute_force(text, likely_words):
+    """Not attractive, but effective and fast enough"""
+    ciphertext = text.upper().replace(" ", "")
+    possibles = []
+    try:
+        for kwlen in range(3, 12):
+            print(kwlen)
+            colsolved = ''.join(rows_from_columns(ciphertext, kwlen))
+            for l in itertools.permutations(range(kwlen)):
+                trans = decrypt_rows(colsolved, l)
+                for w in LIKELY_WORDS:
+                    if w not in trans: 
+                        break
+                else:
+                    print(trans[:30],l, len(possibles))
+                    possibles.append(trans)
+    except KeyboardInterrupt:
+        pass
+    choice = input("Full Text? ")
+    try:
+        n = int(choice)
+        print(possibles[n])
+    except (ValueError, IndexError):
+        print("Bye!")
+
+
+if __name__ == "__main__":
+    ciphertext = """kdIgb oimce idkot seeii huotn pteho mmswi heeoa taoue taebw niInc fnkll flnho unwtd mamtg maera ieanw ronko tsstn kemta rahdn heIpo nsiac oiaeh lntai lbhsh oesoh lhtka rttts aehhI vldTh tunau ehshe noceI aireh rvsrw thett tuaea eseid eeeem topeh eohfB eoodn pafto enect dvasi bpsti xtrrp hfost wiekh Idnpb chrls fidoa atwit urgsd nedao hhgsh iaoeo ricot sgnrg ktedr hanoh tsmao Tehtf morsi ngkhe phtat bmrtt ooeap ctdJt marcs eedad bnier nrhhm rkshg eodBf emahf oeocr ctdfc tvMsy rlneu ooklu nobks nnhhe snosd naenu ueknh noydd Isiwl hseei uibby hatdo ybaeo pAntT sftek atpwa neuoo plttt abtte etioh mrhuh cfsto aeaip otiVc olsgI asstt atyla oodsu ebple rlecs acles hsham ierth ocoen etatn eluhn ehtnw auaih eerje hsaea kshtw cslrh eohin euteI ldelo trele aiutm mesrr oleet setwi naerw aouhe aiawy dycsu tltgu eecni atdte whiks ldlre uoaeg iivsn noleI atorn uscen heuaa tcplr dwato Imath eengb rafal bhade phdgh tgees eniat chdsa dmept oiiku ceuii ereyd ttsun cetTs oieeg naetn sligi tepod legtI oiors rhaaf hgpuc doite tdplh nlell irhpo Scrie eerTo wahto roesi efedu eoanb ahwae Vnnkt oueaS cober seeht rseha rtsda ntmia ahute ebhmI lbnot eyatt ndnht uHoht leade osytl ylwut uoIoa cwmll eints eIoeu retlu ekycb Iuime fnhmi orehv piafe ostto rnhoi lsnne alacn fndof eIhhi cttrh hhltf teipd rTnes ethtp bwenr rpksr mmein tiioa loamr achti deeuw lanse pohhi lidtk achce ynaoh oswum tlond eelaa dmiLd hhrii rhhue tettv eycet aceeo syIhr tcdan orcuo iwvlt wtror Ibdbt ebgal iulrt bduns nswva talsn gfois dytvd tadlk lniwh etcsi kasle yrtda inmen eeacd nluIa hotga ityet erbyt crebc hsead nuIop nolwy daoek iceuw iauan otdaa efood ddhnh octle itehd rfbls adfeo cemla kMkeh Aaiai tmhet udren tiate tuacy adsso eHlai inald oahhA tluwe errre iysta aihko lhymh lelos strmi rnadt asbde dpwdn ennet ttuol wlhni vtoew tidtn ceoeo cfhno rtorh errdt oepmc ceacl heeil eeagn wydnm heaew dnece hngpe tietw wumnp oanee ihitt gsalt hgrhc evett anaaa rtrnl sdenc uagcc dktdo uloyt aucth otnse eaard tdvdr siafn smuey hceha asnke oeett acitn ktnfd nidto dvdee eaone fifim elahr ohnao iweii aatgd sldeI iweuo sncle etrea eaaea oarrt csdal nweet lehwr xeaue eefno ggatt idctt kinet htuss hnctt netot eoanc wrhts rutee ooanh oeehe dlnny tdwed feapw cpdet hnmab ngtyr atnbc anmpl feiet hhdtn obeio asmmn uotio rapre yider rmtbh anlhf timtl klhHa rnobs motfc eefna actfb feerc ysetr ktiAt bwtsf fawod ewyed nyvmm teela idnhn eunae Utnot vtnic hlwpa laato Ilrot stsdk aurrh ianre oroef sepho loyko ltkei rytoi mtoth hIgei rtboi tttre oehde n"""
+    brute_force(ciphertext, LIKELY_WORDS)
+    
